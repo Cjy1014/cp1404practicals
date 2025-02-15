451f3fc2b1f28013bef65f8c567df7a18e75b6d5
caijingyang <caijingyang58@gmail.com>
2025-02-15 20:51:03 +0800

    Generate random numbers using randint, randrange, and uniform

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -7,4 +7,5 @@ print(random.randrange(3, 10, 2)) # line 2: Min = 3, Max = 9, Cannot be 4
 print(random.uniform(2.5, 5.5)) # line 3: Min = 2.5, Max = 5.5
 
 random_number = random.randint(1, 100)
+
 print(random_number)
