dd013f5dbc9a9b67edcc4129824ffdf7622e177f
caijingyang <caijingyang58@gmail.com>
2025-02-15 20:33:42 +0800

    "Generate random numbers with different ranges and types using the random module"

---
+++ prac_03/randoms.py
@@ -0,0 +1,10 @@
+import random
+
+print(random.randint(5, 20)) # line 1: Min = 5, Max = 20
+
+print(random.randrange(3, 10, 2)) # line 2: Min = 3, Max = 9, Cannot be 4
+
+print(random.uniform(2.5, 5.5)) # line 3: Min = 2.5, Max = 5.5
+
+random_number = random.randint(1, 100)
+print(random_number)
