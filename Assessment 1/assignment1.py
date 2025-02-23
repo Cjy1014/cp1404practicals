18097fa1b7f905d907c0d606a22e3c7fa8861a7c
caijingyang <caijingyang58@gmail.com>
2025-02-24 02:43:00 +0800

    Assignment 1

---
+++ Assessment 1/assignment1.py
@@ -0,0 +1,14 @@
+"""
+Name: Jingyang Cai
+Date started: 24/2/2025
+GitHub URL:
+"""
+
+
+def main():
+    """..."""
+    print("Song List 1.0 - by Your Name")
+
+
+if __name__ == '__main__':
+    main()
