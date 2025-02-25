d88bac63690c4d3cf63896150aa5773751705f9e
caijingyang <caijingyang58@gmail.com>
2025-02-26 00:33:38 +0800

    Testing

--- prac_01/test.py
+++ prac_01/test.py
@@ -6,18 +6,18 @@ It also prints the Python version in the run output (may be mixed in with Kivy o
 The Python version should be 3.x (not 2.x).
 """
 
-from kivy.app import App
-from kivy.uix.button import Button
-import sys
-
-
-class CheckSetupApp(App):
-
-    def build(self):
-        return Button(text='hello world')
-
-
-if __name__ == '__main__':
-    # In PyCharm, right-click here and choose 'Run check_setup'
-    print("Python version information:", sys.version)
-    CheckSetupApp().run()
+# from kivy.app import App
+# from kivy.uix.button import Button
+# import sys
+#
+#
+# class CheckSetupApp(App):
+#
+#     def build(self):
+#         return Button(text='hello world')
+#
+#
+# if __name__ == '__main__':
+#     In PyCharm, right-click here and choose 'Run check_setup'
+#     print("Python version information:", sys.version)
+    # CheckSetupApp().run()
