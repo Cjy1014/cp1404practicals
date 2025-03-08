66c54a1de0c40bd132d10498d6f2fcf4f25e9135
caijingyang <caijingyang58@gmail.com>
2025-03-08 23:31:14 +0800

    Add main program to display, add, mark, and save songs

--- Assessment 1/assignment1.py
+++ Assessment 1/assignment1.py
@@ -6,8 +6,30 @@ GitHub URL:
 
 
 def main():
-    """..."""
+    """Allows the user to display, add, mark songs, and save data."""
+    FILE_NAME = "songs.csv"
+    songs = load_songs(FILE_NAME)
+
     print("Song List 1.0 - by Your Name")
+    print(f"{len(songs)} songs loaded.\n")
+
+    choice = ""
+    while choice != "q":
+        print("D - Display songs\nA - Add new song\nC - Complete a song\nQ - Quit")
+        choice = input(">>> ").strip().lower()
+
+        if choice == "d":
+            display_songs(songs)
+        elif choice == "a":
+            add_song(songs)
+        elif choice == "c":
+            mark_song_learned(songs)
+        elif choice == "q":
+            save_songs(FILE_NAME, songs)
+            print("Goodbye!")
+        else:
+            print("Invalid input, please try again.")
+
 
 
 if __name__ == '__main__':
