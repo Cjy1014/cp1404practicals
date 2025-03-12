7ec04eada0dbfa708d35132e0482b24729d6378e
caijingyang <caijingyang58@gmail.com>
2025-03-12 17:16:26 +0800

    Test code for Guitar class methods

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,14 @@
+from guitar import Guitar
+
+def test_guitar():
+    """Test the Guitar class methods."""
+    gibson = Guitar("Gibson L-5 CES", 1992, 16035.40)
+    another_guitar = Guitar("Another Guitar", 2013, 765.4)
+
+    print(f"{gibson.name} get_age() - Expected 102. Got {gibson.get_age()}")
+    print(f"{another_guitar.name} get_age() - Expected 11. Got {another_guitar.get_age()}")
+
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
+
+test_guitar()
